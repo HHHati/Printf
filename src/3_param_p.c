3f2fd71197b2a1b356dd8c1bb7b377902a2fa1e9
Basile De leeuw <bade-lee@fu-r7-p3.s19.be>
2021-11-29 14:43:42 +0100

    7.6

--- src/3_param_p.c
+++ src/3_param_p.c
@@ -6,15 +6,66 @@
 /*   By: bade-lee <bade-lee@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/23 11:18:17 by bade-lee          #+#    #+#             */
-/*   Updated: 2021/11/29 13:20:05 by bade-lee         ###   ########.fr       */
+/*   Updated: 2021/11/29 14:42:23 by bade-lee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
 
+static int	ft_check(char *str)
+{
+	int	n;
+	int	i;
+
+	n = 1;
+	i = ft_strlen(str);
+	if (i == 0 || i == 1)
+	{
+		return (0);
+	}
+	while (n != i)
+	{
+		if (str[n] == '+' || str[n] == '-' || str[n] == str[n + 1])
+		{
+			return (0);
+		}
+		n++;
+	}
+	return (1);
+}
+
+static void	ft_putnbr_base(int nbr, char *base, int *counter)
+{
+	unsigned int	n;
+	unsigned int	i;
+
+	if (ft_check(base) == 1)
+	{
+		i = ft_strlen(base);
+		if (nbr < 0)
+		{
+			n = -nbr;
+			ft_putchar_fd('-', 1);
+			*counter = *counter + 1;
+		}
+		else
+		{
+			n = nbr;
+		}
+		if (n > i - 1)
+		{
+			ft_putnbr_base(n / i, base, counter);
+			*counter = *counter + 1;
+			n %= i;
+		}
+		ft_putchar_fd(base[n], 1);
+	}
+}
+
 int	param_p(int *counter, va_list params)
 {
-	(void) params;
-	(void) counter;
+	const int	nb = (int) va_arg(params, void *);
+
+	ft_putnbr_base(nb, "0123456789abcdef", counter);
 	return (1);
 }
